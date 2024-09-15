005bea8cb2caa3124c7f373078647aadb2db7150
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-09-15 15:55:32 +0800

    initial commit

---
+++ number.js
@@ -0,0 +1,35 @@
+// String to Number
+console.log("Convert String to Number");
+let price = "200";
+
+// parseInt
+let price1 = parseInt(price);
+console.log("parseInt:", price1);
+
+// Number()
+let price2 = Number(price);
+console.log("Number:", price2);
+
+// + operator
+let priceInt = +price;
+console.log("+price:", priceInt);
+
+// Number to String
+console.log("\nConvert Number to String");
+let distance = 150;
+
+// .toString()
+let distance1 = distance.toString();
+console.log("distance.toString():", distance1);
+
+// String
+let distance2 = String(distance);
+console.log("String:", distance2);
+
+// String to Decimal Number
+console.log("\nConvert String to Decimal Number");
+let weight = "72.5";
+
+// parseFloat
+let weightFloat = parseFloat(weight);
+console.log("parseFloat:", weightFloat);
